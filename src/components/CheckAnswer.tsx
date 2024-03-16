f549c70fa0b0f57d4cc46b4c4eabab655aba5554
Trung Nguyen <trungtng@udel.edu>
2024-03-16 11:23:55 -0400

    finished forms

--- src/components/CheckAnswer.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import { Form } from "react-bootstrap";
-
-export function CheckAnswer({ expectedAnswer }: { expectedAnswer: string }) {
-    const [answer, setAnswer] = useState<string>("");
-    return <div></div>;
-}
